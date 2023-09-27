2fe8b7df0bb5c382c66d197893d2f86653380251
HabibaSabrina <habibasabrina92@gmail.com>
2023-09-27 18:14:37 +0600

    color is changed

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -8,14 +8,14 @@ const Banner = () => {
         AOS.init();
       }, [])
     return (
-        <div className='bg-yellow-200 flex items-center gap-20 '>
+        <div className='bg-[#BAD7E9] flex items-center gap-20 '>
             {/* Main image of the banner */}
             <img className='ml-20 mt-auto' src="/bannerP.png" alt="" />
             {/* Right portion of the banner */}
-            <div className='bg-purple-300 p-10 ' data-aos="fade-left" data-aos-duration={1000}>
-                <h1 className='text-4xl font-bold text-blue-900'>Welcome to ShopMart</h1>
-                <p className='text-gray-700 my-5'>ShopMart, the ultimate e-commerce platform, offers a diverse range of products, easy navigation, and secure transactions. Find top brands, competitive prices, and swift delivery, making your online shopping experience a breeze. Shop smart, shop with ShopMart today!</p>
-                <button className='bg-red-600 text-white font-semibold p-3 px-10 rounded'>Shop Now</button>
+            <div className='bg-[#2B3467] p-10 ' data-aos="fade-left" data-aos-duration={1000}>
+                <h1 className='text-4xl font-bold text-white'>Welcome to ShopMart</h1>
+                <p className='text-gray-200 my-5'>ShopMart, the ultimate e-commerce platform, offers a diverse range of products, easy navigation, and secure transactions. Find top brands, competitive prices, and swift delivery, making your online shopping experience a breeze. Shop smart, shop with ShopMart today!</p>
+                <button className='bg-[#EB455F] text-white font-semibold p-3 px-10 rounded'>Shop Now</button>
                 {/* banner cards */}
                 <div className='flex gap-10 text-center mt-10'>
                 <div className='bg-white p-3 animate-pulse hover:animate-none'>
