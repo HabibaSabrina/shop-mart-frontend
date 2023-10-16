b7d4499a358a82305767a968d1a23cba1d5dadea
HabibaSabrina <habibasabrina92@gmail.com>
2023-10-17 04:50:36 +0600

    scrolling fixed

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -4,14 +4,18 @@ import { Outlet } from 'react-router-dom';
 import Footer from '../components/Shared/Footer';
 import Header from '../components/Shared/Header/Header';
 import AosInitializer from '../animation/AosInitializer';
+import ScrollToTop from '../ScrollToTop/ScrollToTop';
 
 const Main = () => {
     return (
+        <div>
+            <ScrollToTop></ScrollToTop>
         <div className='font-sans'>
             <AosInitializer></AosInitializer>
             <Header></Header>
             <Outlet></Outlet>
             <Footer></Footer>
+        </div>
         </div>
     );
 };
